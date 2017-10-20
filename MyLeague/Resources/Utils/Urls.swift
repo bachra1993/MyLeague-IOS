ad1a68eae8577e66175dea824f303b7060e1d6ff
bechir kaddech <bechir.kaddech@esprit.tn>
2017-10-20 20:24:26 +0100

    version patch update

--- MyLeague/Resources/Utils/Urls.swift
+++ MyLeague/Resources/Utils/Urls.swift
@@ -12,12 +12,12 @@ import Foundation
 class Urls: NSObject {
     
    static let APIKEY : String = "RGAPI-7f825a26-8594-4983-a74a-b08e84325482"
-   static let CHAMPIONS_LIST : String = "http://ddragon.leagueoflegends.com/cdn/6.24.1/data/en_US/champion.json"
+   static let CHAMPIONS_LIST : String = "http://ddragon.leagueoflegends.com/cdn/7.20.3/data/en_US/champion.json"
    static let CHAMPIONS_LIST_FREE : String = "https://euw1.api.riotgames.com/lol/platform/v3/champions?freeToPlay=false"
     
     
     
-    static let CHAMPION_PICTURE : String = "https://ddragon.leagueoflegends.com/cdn/7.10.1/img/champion/"
+    static let CHAMPION_PICTURE : String = "https://ddragon.leagueoflegends.com/cdn/7.20.3/img/champion/"
     
     
     
